4f509b4546d0ffafba5af36aa349ef9d5c491d5e
Kinyua <alfkinyua@gmail.com>
2022-12-13 20:38:18 +0300

    Add js query selectors

---
+++ style.js
@@ -0,0 +1,4 @@
+const one = document.querySelectorAll('.one');
+const modal = document.querySelector('.modal');
+const modal_close = document.querySelector('.modal_close');
+const overlay = document.querySelector('.overlay');
