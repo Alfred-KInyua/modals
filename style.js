1a06eb4c8b293faaddcb36ba7215a25192328abe
Kinyua <alfkinyua@gmail.com>
2022-12-14 10:37:17 +0300

    refactor code

--- style.js
+++ style.js
@@ -1,4 +1,22 @@
 const one = document.querySelectorAll('.one');
 const modal = document.querySelector('.modal');
-const modal_close = document.querySelector('.modal_close');
+const modal_close = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+
+const openMOdal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < one.length; i++) {
+  one[i].addEventListener('click', openMOdal);
+}
+modal_close.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+const close = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+modal_close.addEventListener('click', close);
+overlay.addEventListener('click', close);
